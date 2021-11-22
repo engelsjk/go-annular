adc93ab36779250c61f9609ace07948eb828c88f
engelsjk <jon.engelsman@gmail.com>
2021-11-21 18:32:59 -0800

    clean up vars

--- annular.go
+++ annular.go
@@ -15,20 +15,19 @@ import (
 )
 
 var (
-	width              = 1000
-	height             = 1000
-	radial_center_base = 50.0
-	arc_length_base    = 50.0
-	radial_length_base = 50.0
-	n_base             = 15000
-	palettesFilename   = "palettes.json"
+	width            = 1000
+	height           = 1000
+	maxRadialCenter  = 0.10
+	maxArcLength     = 0.05
+	maxRadialLength  = 0.05
+	maxN             = 15000
+	palettesFilename = "palettes.json"
 )
 
 func Run(w io.Writer) {
 
 	// seed
 	seed := time.Now().Unix()
-	seed = 1560532121
 	rand.Seed(seed) // initialize global pseudo random generator
 
 	// colors
@@ -48,24 +47,24 @@ func Run(w io.Writer) {
 	s.Rect(0, 0, width, height, s.RGB(0, 0, 0))
 
 	// randomize parameters
-	radial_center := rand.Float64() * radial_center_base //px
+	radial_center := rand.Float64() * maxRadialCenter * float64(width) //px
 	cx, cy := rand.Float64()*float64(width), rand.Float64()*float64(height)
 	stype := rand.Intn(4)
-	n := rand.Intn(n_base)
+	n := rand.Intn(maxN)
 
 	// annuli
 	for i := 0; i < n; i++ {
 
-		arc_start := math.Mod(rand.Float64()*360.0/180.0*math.Pi, 2*math.Pi)
-		radial_start := radial_center + rand.Float64()*(math.Sqrt(2)*float64(width))
+		arcStart := math.Mod(rand.Float64()*360.0/180.0*math.Pi, 2*math.Pi)
+		radialStart := radial_center + rand.Float64()*(math.Sqrt(2)*float64(width))
 
-		radial_length := radialLength(radial_start, radial_length_base, arc_start, stype) //px
-		arc_length := arcLength(arc_start, arc_length_base, radial_start, stype)
+		radialLength := radialLength(radialStart, maxRadialCenter*float64(width), arcStart, stype) //px
+		arcLength := arcLength(arcStart, maxArcLength*float64(width), radialStart, stype)
 
-		arc_end := math.Mod(arc_start+arc_length/180.0*math.Pi, 2*math.Pi)
-		radial_end := radial_start + radial_length
+		arc_end := math.Mod(arcStart+arcLength/180.0*math.Pi, 2*math.Pi)
+		radial_end := radialStart + radialLength
 
-		annulus := Annulus{x: cx, y: cy, start: arc_start, end: arc_end, inner: radial_start, outer: radial_end}
+		annulus := Annulus{x: cx, y: cy, start: arcStart, end: arc_end, inner: radialStart, outer: radial_end}
 
 		path := annulus.path()
 		fill := fmt.Sprintf("fill:%s", palette[rand.Intn(len(palette))])
